95927949c8b27d77f43e9833b20186541b2408f2
singhanshuman01 <singhanshuman592@gmail.com>
2026-07-19 03:37:38 +0530

    Few bugs fixed

--- rx.v
+++ rx.v
@@ -45,7 +45,7 @@ end
 always @(posedge clk or negedge rst) begin
     if(!rst) shift_reg <= 0;
     else begin
-        if(state == RECEIVE && baud_counter==15) shift_reg <= {shift_reg, rx};
+        if(state == RECEIVE && baud_counter==15) shift_reg <= {rx, shift_reg[7:1]};
         else shift_reg <= shift_reg;
     end
 end
@@ -56,7 +56,7 @@ always @(*) begin
         IDLE: {wdata, wen} = 0;
         START: {wdata, wen} = 0;
         RECEIVE: {wdata, wen} = 0;
-        STOP: if(!full) {wdata, wen} = {shift_reg, 1'b1};
+        STOP: if(!full && baud) {wdata, wen} = {shift_reg, 1'b1};
         default: {wdata, wen} = 0;
     endcase
 end
@@ -70,7 +70,7 @@ always @(posedge clk or negedge rst) begin
         if(state==START && baud_counter==7) baud_counter <= 0;
         else baud_counter <= baud_counter + 1'b1;
 
-        if(state == RECEIVE && baud_counter==15) bit_counter = bit_counter+1'b1;
+        if(state == RECEIVE && baud_counter==15) bit_counter <= bit_counter+1'b1;
     end
 end
 
